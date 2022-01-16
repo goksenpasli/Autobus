71f4bc51ac249ef91ec4b1f39248344a0612d099
goksenpasli <gpasli@gmail.com>
2022-01-16 21:24:28 +0300

    Add project files.

--- Autobus/Converter/KoltukNoVisibilityConverter.cs
+++ Autobus/Converter/KoltukNoVisibilityConverter.cs
@@ -2,16 +2,20 @@
 using System;
 using System.Globalization;
 using System.Linq;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Autobus.Converter
 {
-    public sealed class KoltukNoDisabledConverter : IMultiValueConverter
+    public sealed class KoltukNoVisibilityConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return values[0] is int koltukno && values[1] is Sefer seçilisefer && values[2] is int müşterikoltuk
-                && seçilisefer?.Müşteri.Any(z => z.KoltukNo == koltukno) != true;
+            if (values[0] is int koltukno && values[1] is Sefer seçilisefer && values[2] is int müşterikoltuk)
+            {
+                return seçilisefer?.Müşteri.Any(z => z.KoltukNo == koltukno) == true ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Visible;
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
